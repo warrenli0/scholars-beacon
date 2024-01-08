4fe7ad9ca735ff2f685b04d1ee7ea0e87f6b5b2f
Aniket Nayak <aniketn@terpmail.umd.edu>
2024-01-07 20:32:59 -0800

    explanation v1 done

--- beta-mcq/src/App.js
+++ beta-mcq/src/App.js
@@ -20,6 +20,7 @@ function App() {
         { id: 3, text: "1.08(99.95 + 5)x", isCorrect: false },
       ],
       type: "Math (no calc)",
+      explanation: "The room rate is $99.95 for every night. Since you stay for x nights, the price becomes 99.95x. However, there is a tax of 8% on the room rate, so the pice is 0.08(99.95x). Lastly, with the addition of a one time untaxed fee, the answer is the second option — 1.08(99.95x) + 5.",
     },
     {
       text: "Given y = a(x-2)(x+4) and a is a nonzero constant: The graph of the equation in the xy-plane is a parabola with vertex (c,d), Which of the following is equal to d?",
@@ -30,6 +31,7 @@ function App() {
         { id: 3, text: "-2a", isCorrect: false },
       ],
       type: "Math (no calc)",
+      explanation: "TBD 1",
     },
     {
       text: "At a lunch stand, each hamburger has 50 more calories than each order of fries. If 2 hamburgers and 3 orders of fries have a total of 1700 calories, how many calories does a hamburger have?",
@@ -40,6 +42,7 @@ function App() {
         { id: 3, text: "warren", isCorrect: false },
       ],
       type: "Math (no calc)",
+      explanation: "TBD 2",
     },
     {
       text: "At Luffy's High School, approximately 7 percent of enrolled juniors and 5 percent of enrolled seniors were inducted into the National Pirate Society last year. If there were 562 juniors and 602 seniors enrolled at Luffy's High School last year, which is closest to the total number of juniors and seniors at Luffy's High School last year who were inducted into the National Pirate Society?",
@@ -50,9 +53,10 @@ function App() {
         { id: 3, text: "39", isCorrect: false },
       ],
       type: "Math (calc)",
+      explanation: "TBD 3",
     },
     {
-      text: "Nate walks 25 meters in 13.7 seconds. If he walks at this same rate, which of the following is closest to the distance he will walk in 4 minutes?",
+      text: "Neal walks 25 meters in 13.7 seconds. If he walks at this same rate, which of the following is closest to the distance he will walk in 4 minutes?",
       options: [
         { id: 0, text: "150 meters", isCorrect: false },
         { id: 1, text: "450 meters", isCorrect: true },
@@ -60,6 +64,7 @@ function App() {
         { id: 3, text: "1000 meters", isCorrect: false },
       ],
       type: "Math (calc)",
+      explanation: "TBD 4",
     },
   ];
 
@@ -84,48 +89,95 @@ function App() {
     }
   }
 
+  const nextReviewQ = () => {
+    setQuestion(question + 1);
+  }
+
   // begin review
   const startReview = () => {
-    setAnswers([]);
-    setScore(0);
+    //setAnswers([]);
+    //setScore(0);
+    console.log(answers);
     setQuestion(0);
     setResults(false);
     setReview(true);
   };
 
-  return (
-    <div className="App">
+  // not sure why CSS not being inherited so having to do like this for some classes to save time
+  const ecstyle = {
+    color: "black",
+    backgroundColor: "white",
+    width: "40%",
+    padding: "16px",
 
-    <h1 className="timer">SB</h1>
+    position: "relative",
+
+    borderRadius: "22px",
+    boxShadow: "rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px",
     
-      { showResults ? (
-        /* results card */
-        <div className="final-results">
-          <h1>Results</h1>
-          <h2>
-            {score} out of {questions.length} correct
-          </h2>
-          <ul>
-            {answers.map((resp) => {
-              if (resp.response) {
-                return (
-                  <li style={{color: "#33FF00"}} key={resp.id}>Question {resp.id+1} <span style={{color: "white"}}>{questions[resp.id].type}</span></li>
-                );
-              } else {
-                return (
-                  <li style={{color: "#FF0001"}} key={resp.id}>Question {resp.id+1} <span style={{color: "white"}}>{questions[resp.id].type}</span></li>
-                );
-              }
-            })}
-            
-            
-          </ul>
-          <button onClick={() => startReview()}>Start Review</button>
-        </div>
-      ) : (
-        /* timer */
+  };
+  
+  
+  const rcstyle = {
+    justifyContent: "center",
+    display: "flex",
+  };
 
-        /* question card */
+  //action={understand(question)} in form
+  function ECard() {
+    return (
+      <div style={ecstyle}>
+        <h2>Explanation</h2>
+        <h3 style={{fontWeight: "400"}}>{questions[question].explanation}</h3>
+        <div className="checkandnext">
+          <form >
+            <input type="checkbox" name="cb" />
+            <label for="cb" style={{fontWeight: "600", fontSize:"18.72px", verticalAlign: "middle"}}>I understand this problem</label>
+          </form>
+          <p style={{color:"#858585"}}>Don't fully understand? Skip for now!</p>
+        </div>
+        <span onClick={() => nextReviewQ()} style={{position: "absolute", fontSize:"50px", float:"right", bottom: "20px", right:"20px", cursor: "pointer"}}>&#8658;</span> 
+        
+      </div>
+    )
+  }
+
+  // qustion card component
+  function QCard() {
+    /* question card */
+    if (showReview) {
+
+      return (
+        <div div style={rcstyle}>
+          <div className="review-question-card">
+            <h2 className="review-questionNum">Review Q{question+1} — {questions[question].type}</h2>
+            <h3>{questions[question].text}</h3>
+            <ul>
+              {questions[question].options.map((option) => {
+                if (option.isCorrect) {
+                  return (
+                    <li style={{backgroundColor: "#33FF00"}} key={option.id}>{option.text}</li>
+                  );
+                } else {
+                  if (answers[question].choice === option.id) {
+                    return (
+                      <li style={{backgroundColor: "#FF0001"}} key={option.id}>{option.text}</li>
+                    );
+                  }
+                  return (
+                    <li key={option.id}>{option.text}</li>
+                  );
+                }
+              })}
+            </ul>
+          </div>
+        <ECard />
+        </div>
+      )
+
+    } else {
+
+      return (
         <div className="question-card">
           <h2 className="questionNum">Q{question+1} — {questions[question].type}</h2>
           <h3>{questions[question].text}</h3>
@@ -136,7 +188,44 @@ function App() {
               );
             })}
           </ul>
-        </div>
+      </div>)
+
+    }
+    
+  }
+
+  return (
+    <div className="App">
+    
+
+    <h1 className="timer">SB</h1>
+
+    
+      { showResults ? (
+        /* results card */
+        <div className="final-results">
+        <h1>Results</h1>
+        <h2>
+          {score} out of 5 correct
+        </h2>
+        <ul>
+          {answers.map((resp) => {
+            if (resp.response) {
+              return (
+                <li style={{color: "#33FF00"}} key={resp.id}>Question {resp.id+1} <span style={{color: "white"}}>{questions[resp.id].type}</span></li>
+              );
+            } else {
+              return (
+                <li style={{color: "#FF0001"}} key={resp.id}>Question {resp.id+1} <span style={{color: "white"}}>{questions[resp.id].type}</span></li>
+              );
+            }
+          })}
+        </ul>
+        <button onClick={() => startReview()}>Start Review</button>
+      </div>
+      ) : (
+
+        <QCard />
 
       )}
       
