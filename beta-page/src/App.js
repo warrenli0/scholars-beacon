c677617561d22e897539ba10411f44f83dc5c87a
Aniket Nayak <aniketn@terpmail.umd.edu>
2024-02-07 14:34:36 -0800

    mobile fade in change

--- beta-page/src/App.js
+++ beta-page/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import LandingPage from "./components/LandingPage";
 import WaveTransition from "./components/WaveTransition";
 import Start from "./components/Start";
+import Main from "./components/Main";
 
 function App() {
   // Properties
   const [showLP, setshowLandingPage] = useState(true);
   const [showTopWave, setShowTopWave] = useState(0);
   const [wavesFinished, setWavesFinished] = useState(true);
+  const [showMain, setshowMain] = useState(false);
 
   /*
   if (showLandingPage) {
@@ -21,6 +23,7 @@ function App() {
       <LandingPage setShowTopWave={setShowTopWave} showLP={showLP} setshowLandingPage={setshowLandingPage} setWavesFinished={setWavesFinished}/>
       <WaveTransition showTopWave={showTopWave} wavesFinished={wavesFinished} setWavesFinished={setWavesFinished}/>
       <Start showLP={showLP}/>
+      <Main showMain={showMain}/>
     </div>
   )
 }
---
+++ beta-page/src/components/Main.js
@@ -0,0 +1,16 @@
+import './Main.css'
+import peng from '../images/ping-bubble.png';
+
+export default function Main({showMain}) {
+    if (showMain) {
+        return (
+            <div>
+    
+                <h1>Hellooooo</h1>
+    
+            </div>
+        )
+    }
+
+};
+  
