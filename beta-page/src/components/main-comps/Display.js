94afcf89b282822d34b907b126ad9ac8bc43fefe
Aniket Nayak <aniketn@terpmail.umd.edu>
2024-02-12 13:20:16 -0800

    qcards actually done

--- beta-page/src/components/main-comps/Display.js
+++ beta-page/src/components/main-comps/Display.js
@@ -1,41 +1,8 @@
-import show_icon from '../../images/Show.png'
-import hide_icon from '../../images/Hide.png'
-import TheNotepad from './TheNotepad';
-import TheQcard from './TheQcard';
+import QGrid from './QGrid';
 
 import React, { useState } from "react";
 
-function Timer({show}) {
-  // measures total seconds passed
-  const [seconds, setSeconds] = useState(-4);
-  var showseconds = '';
-  var showminutes = '00';
-
-  setTimeout(function(){
-    setSeconds(seconds + 1);
-  }, 1000);
-
-  if (seconds % 60 < 10) {
-    showseconds = '0' + seconds % 60;
-  } else if (seconds % 60 == 0) {
-    showseconds = '00';
-  } else {
-    showseconds = '' + seconds % 60;
-  }
-
-  showminutes = Math.floor(seconds / 60);
-  if (showminutes < 10) {
-    showminutes = '0' + showminutes;
-  }
-
-  return (
-    <h1 show={show}>{showminutes}:{showseconds}</h1>
-  )
-};
-
 export default function Display({questions, showQCards, setshowQCards, notesArray, setnotesArray}) {
-    const [currQIndex, setcurrQIndex] = useState(0);
-    const [showIcon, setshowIcon] = useState('1');
     const [bgNum, setbgNum] = useState(0);
 
     /* close condition
@@ -59,29 +26,7 @@ export default function Display({questions, showQCards, setshowQCards, notesArra
                   
                 </div>
 
-                <div className='question-grid' move={+bgNum}> {/* Used to position misc + qcards */}
-                    <div className='qcard-header' move={+bgNum}> 
-                        <h1>Q.{currQIndex+1}</h1>
-                        <h3><i>{questions[currQIndex].type}</i></h3>
-                    </div>
-                    <div className='qcard-question-type' move={+bgNum}>
-                      <h3><i>{questions[currQIndex].type}</i></h3>
-                    </div>
-                    <div className='qcard-misc' move={+bgNum}> 
-                        <img src={show_icon} id='show_icon' show={showIcon} onClick={() => {setshowIcon('0')}}/>
-                        <img src={hide_icon} id='hide_icon' show={showIcon} onClick={() => {setshowIcon('1')}}/>
-                        <Timer show={showIcon}/>
-                    </div>
-                    <div className='qcard-version-note' move={+bgNum}>
-                      <h3>experience the full version on a larger screen</h3>
-                    </div>
-                    <div className='qcard-notepad' move={+bgNum}> 
-                        <TheNotepad currQIndex={currQIndex} notesArray={notesArray} setnotesArray={setnotesArray} calc={''+(questions[currQIndex].type == 'Math (calc)')} bgNum={bgNum}/>
-                    </div>
-                    <div className='qcard-container'> {/* qcard */}
-                        <TheQcard prob={questions[currQIndex]} bgNum={bgNum} setbgNum={setbgNum} currQIndex={currQIndex} setcurrQIndex={setcurrQIndex}/>
-                    </div>
-                  </div>
+                <QGrid questions={questions} notesArray={notesArray} setnotesArray={setnotesArray} bgNum={bgNum} setbgNum={setbgNum}/>
 
             </div>
         )
---
+++ beta-page/src/components/main-comps/QGrid.js
@@ -0,0 +1,74 @@
+import show_icon from '../../images/Show.png'
+import hide_icon from '../../images/Hide.png'
+import TheNotepad from './TheNotepad';
+import TheQcard from './TheQcard';
+
+import React, { useState } from "react";
+
+function Timer({show}) {
+    // measures total seconds passed
+    const [seconds, setSeconds] = useState(-4);
+    var showseconds = '';
+    var showminutes = '00';
+  
+    setTimeout(function(){
+      setSeconds(seconds + 1);
+    }, 1000);
+  
+    if (seconds % 60 < 10) {
+      showseconds = '0' + seconds % 60;
+    } else if (seconds % 60 == 0) {
+      showseconds = '00';
+    } else {
+      showseconds = '' + seconds % 60;
+    }
+  
+    showminutes = Math.floor(seconds / 60);
+    if (showminutes < 10) {
+      showminutes = '0' + showminutes;
+    }
+  
+    return (
+      <h1 show={show}>{showminutes}:{showseconds}</h1>
+    )
+  };
+
+export default function QGrid({questions, notesArray, setnotesArray, bgNum, setbgNum}) {
+    const [showQgrid, setshowQgrid] = useState(true);
+    const [currQIndex, setcurrQIndex] = useState(0);
+    const [showIcon, setshowIcon] = useState('1');
+
+    if (bgNum == 5) { // last question done
+        setTimeout(function(){
+            setshowQgrid(false); // remove qcard after everything goes away
+        }, 1500);
+    }
+
+    if (showQgrid) {
+        return (
+            <div className='question-grid' move={+bgNum}> {/* Used to position misc + qcards */}
+                    <div className='qcard-header' move={+bgNum}> 
+                        <h1>Q.{currQIndex+1}</h1>
+                        <h3><i>{questions[currQIndex].type}</i></h3>
+                    </div>
+                    <div className='qcard-question-type' move={+bgNum}>
+                      <h3><i>{questions[currQIndex].type}</i></h3>
+                    </div>
+                    <div className='qcard-misc' move={+bgNum}> 
+                        <img src={show_icon} id='show_icon' show={showIcon} onClick={() => {setshowIcon('0')}}/>
+                        <img src={hide_icon} id='hide_icon' show={showIcon} onClick={() => {setshowIcon('1')}}/>
+                        <Timer show={showIcon}/>
+                    </div>
+                    <div className='qcard-version-note' move={+bgNum}>
+                      <h3>experience the full version on a larger screen</h3>
+                    </div>
+                    <div className='qcard-notepad' move={+bgNum}> 
+                        <TheNotepad currQIndex={currQIndex} notesArray={notesArray} setnotesArray={setnotesArray} calc={''+(questions[currQIndex].type == 'Math (calc)')} bgNum={bgNum}/>
+                    </div>
+                    <div className='qcard-container'> {/* qcard */}
+                        <TheQcard prob={questions[currQIndex]} bgNum={bgNum} setbgNum={setbgNum} currQIndex={currQIndex} setcurrQIndex={setcurrQIndex}/>
+                    </div>
+            </div>
+        )
+    }
+}
