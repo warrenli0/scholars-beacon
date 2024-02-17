417e7ef1954abe920506afc7b189689e294a479a
Aniket Nayak <aniketn@terpmail.umd.edu>
2024-02-16 18:52:43 -0800

    thumbs

--- beta-page/src/components/main-comps/TheEcard.js
+++ beta-page/src/components/main-comps/TheEcard.js
@@ -1,13 +1,43 @@
 import arrow from "../../images/Arrow.png"
-import disc from "../../images/gray-discord.png"
+import disc from "../../images/blue-discord.png"
+import thumbs_up from "../../images/thumbs-up.png"
+import green_thumbs_up from "../../images/green-thumbs-up.png"
+import thumbs_down from "../../images/thumbs-down.png"
+import red_thumbs_down from "../../images/red-thumbs-down.png"
 import React, { useState, useRef } from "react";
 
 export default function TheEcard({prob, bgNum, setbgNum, currQIndex, setcurrQIndex, chosenAnswers}) {
     const [showCard, setshowCard] = useState(true);
     const [selectedChoice, setselectedChoice] = useState('0');
     const [exit, setexit] = useState('0');
+    const [thumbUp, setthumbUp] = useState('0');
+    const [thumbDown, setthumbDown] = useState('0');
 
     function nextQ() {
+        
+    }
+    function clickThumbs() {
+        // v:0 black thumbs up to green
+        if (thumbUp == '0') {
+            setthumbUp('1');
+            if (thumbDown == '1') {
+                setthumbDown('0');
+            }
+        }
+        else if (thumbUp == '1') {
+            setthumbUp('0');
+        }
+    }
+    function clickThumbsDown() {
+        if (thumbDown == '0') {
+            setthumbDown('1');
+            if (thumbUp == '1') {
+                setthumbUp('0');
+            }
+        }
+        else if (thumbDown == '1') {
+            setthumbDown('0');
+        }
     }
 
     // check here to see which format to display qcard in
@@ -46,6 +76,10 @@ export default function TheEcard({prob, bgNum, setbgNum, currQIndex, setcurrQInd
                     <div className="the-e-explanation">
                         <h2><b>Explanation</b></h2>
                         <h2>{prob.explanation}</h2> 
+                        <img src={thumbs_up} className="thumbs-up" version={thumbUp} onClick={() => {clickThumbs()}}/>
+                        <img src={green_thumbs_up} className="green-thumbs-up" version={thumbUp} onClick={() => {clickThumbs()}}/>
+                        <img src={thumbs_down} className="thumbs-down" version={thumbDown} onClick={() => {clickThumbsDown()}}/>
+                        <img src={red_thumbs_down} className="red-thumbs-down" version={thumbDown} onClick={() => {clickThumbsDown()}}/>
                     </div>
                     <div className="the-right-e-line"><div className="the-left-e-real-line"></div></div>
                     <div className="the-e-check">
@@ -53,7 +87,7 @@ export default function TheEcard({prob, bgNum, setbgNum, currQIndex, setcurrQInd
                         <label><h2>I understand this problem</h2></label>
                     </div>
                     <div className="the-e-help">
-                        <h4>Don’t understand? Skip {"&"} ask for help in the discord</h4>
+                        <h4>Don't understand? Skip {"&"} ask for help in the discord!</h4>
                         <img src={disc} />
                         <img src={arrow} id="the-e-arrow" onClick={() => {nextQ()}}/>
                     </div>
