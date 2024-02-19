3fdf08c447af88620f553c0cff2ccf1115d631a5
Aniket Nayak <aniketn@terpmail.umd.edu>
2024-02-19 14:06:58 -0800

    ecards v1 done

--- beta-page/src/components/main-comps/TheEcard.js
+++ beta-page/src/components/main-comps/TheEcard.js
@@ -128,7 +128,51 @@ export default function TheEcard({prob, bgNum, setbgNum, currQIndex, setcurrQInd
         } else if (prob.has_img) { // has sameish format as reading
             return (
                 <div className='the-ecard' exit={exit} mobile={mobile} format='Image'>
-
+                    <div className="the-e-question" mobile={mobile} format='Image'>
+                        <h2>{prob.text}</h2>
+                    </div>
+                    <div className="the-left-e-line" format='Image'><div className="the-left-e-real-line"></div></div>
+                    <div className="answer-choice" id="e-choice1" mobile={mobile} format='Image'>
+                        <button className="the-e-button" choice="c1" chosen={+(chosenAnswers[0][1])} bg={""+(prob.options[0].isCorrect)}><p>{prob.options[0].text}</p></button>
+                    </div>
+                    <div className="answer-choice" id="e-choice2" mobile={mobile} format='Image'>
+                        <button className="the-e-button" choice="c2" chosen={+(chosenAnswers[0][1])} bg={""+(prob.options[1].isCorrect)}><p>{prob.options[1].text}</p></button>
+                    </div>
+                    <div className="answer-choice" id="e-choice3" mobile={mobile} format='Image'>
+                        <button className="the-e-button" choice="c3" chosen={+(chosenAnswers[0][1])} bg={""+(prob.options[2].isCorrect)}><p>{prob.options[2].text}</p></button>
+                    </div>
+                    <div className="answer-choice" id="e-choice4" mobile={mobile} format='Image'>
+                        <button className="the-e-button" choice="c4" chosen={+(chosenAnswers[0][1])} bg={""+(prob.options[3].isCorrect)}><p>{prob.options[3].text}</p></button>
+                    </div>
+                    <div className="the-e-line" format='Image'><div className="the-e-real-line"></div></div>
+                    <div className="the-e-image" mobile={mobile} format='Image'>
+                        <img src={prob.img_link} mobile={mobile} format='Image'/>
+                    </div>
+                    <div className="the-e-explanation" mobile={mobile} format="Image">
+                        <h2><b>Explanation</b></h2>
+                        <h2>{prob.explanation}</h2> 
+                        <img src={thumbs_up} className="thumbs-up" format="Image" version={thumbUp} mobile={mobile} onClick={() => {clickThumbs()}}/>
+                        <img src={green_thumbs_up} className="green-thumbs-up" format="Image" version={thumbUp} onClick={() => {clickThumbs()}}/>
+                        <img src={thumbs_down} className="thumbs-down" format="Image" version={thumbDown} onClick={() => {clickThumbsDown()}}/>
+                        <img src={red_thumbs_down} className="red-thumbs-down" format="Image" version={thumbDown} onClick={() => {clickThumbsDown()}}/>
+                    </div>
+                    <div className="the-right-e-line" format="Image"><div className="the-left-e-real-line"></div></div>
+                    <div className="the-e-check" mobile={mobile} format="Image">
+                        <input type="checkbox" id="e-understood" name="check" checked={checked} onChange={() => {setchecked(!checked)}}/>
+                        <label><h2>I understand this problem</h2></label>
+                    </div>
+                    <div className="the-e-help" mobile={mobile} format="Image">
+                        <h4>Don't understand? Skip {"&"} ask for help in the discord!</h4>
+                        <img src={disc} />
+                        <img src={arrow} id="the-e-arrow" onClick={() => {nextQ()}}/>
+                    </div>
+                    <div className="the-e-see" mobile={mobile}>
+                        <h3 onClick={() => {clickMobile()}}>See Explanation</h3>
+                    </div>
+                    <div className="the-e-back" mobile={mobile}>
+                        <h3 onClick={() => {clickBack()}}>Back</h3>
+                        <img src={arrow} id="the-e-arrow" onClick={() => {nextQ()}}/>
+                    </div>
                 </div>
             )
         } else { // default format
